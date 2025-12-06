2ab2d45e61bcf7c0f47d57b971754ff999a7e3a1
Mihai-Dragos Ungureanu <m.ungureanu1@student.tue.nl>
2025-12-06 22:24:56 +0100

    New iteration
    -add log in page

---
+++ user.js
@@ -0,0 +1,29 @@
+// ---------------------------------------------------------
+// TRIPSYNAPSE USER DATABASE
+// ---------------------------------------------------------
+// This file acts as a simple database for the Demo Page.
+// You can add as many users as you like to this list.
+// ---------------------------------------------------------
+
+const VALID_USERS = [
+    {
+        username: "admin",
+        password: "password123", // Change this for security!
+        name: "Admin User"
+    },
+    {
+        username: "demo",
+        password: "demoUser2025",
+        name: "Demo Traveler"
+    },
+    {
+        username: "mihai",
+        password: "dev",
+        name: "Mihai-Dragoș"
+    },
+    {
+        username: "bogdan",
+        password: "guru",
+        name: "Bogdan"
+    }
+];
